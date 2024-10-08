81a98696675b8ac67706e49469c2b99ad521c75f
WiskerJan <72862988+Jan-Kang@users.noreply.github.com>
2024-10-08 15:19:43 +0200

    FixNode and CollisionBehavior

--- artisynth_model_lowerlimb/src/lowerlimb/lowerlimb.java
+++ artisynth_model_lowerlimb/src/lowerlimb/lowerlimb.java
@@ -2,10 +2,17 @@ package lowerlimb;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import artisynth.core.femmodels.AnsysCdbReader;
+import artisynth.core.femmodels.FemFactory;
 import artisynth.core.femmodels.FemModel.Ranging;
 import artisynth.core.femmodels.FemModel.SurfaceRender;
 import artisynth.core.femmodels.FemModel3d;
+import artisynth.core.femmodels.FemNode3d;
+import artisynth.core.femmodels.NodeNumberReader;
+import artisynth.core.materials.LinearMaterial;
+import artisynth.core.mechmodels.CollisionBehavior;
 import artisynth.core.mechmodels.MechModel;
 import artisynth.core.renderables.ColorBar;
 import artisynth.core.workspace.RootModel;
@@ -20,9 +27,8 @@ public class lowerlimb extends RootModel {
 	String geodata = PathFinder.getSourceRelativePath(this, "data/");
 	
 	//create mech
-	MechModel mech_femur;
-	MechModel mech_tifi;
-	
+	MechModel mech = new MechModel();
+
 	//create fem
     FemModel3d femur;
     FemModel3d tifi;
@@ -31,9 +37,6 @@ public class lowerlimb extends RootModel {
 	public void build(String[] args) throws IOException {
 		
 		//import lowerlimb_mesh_data
-		mech_femur = new MechModel("mech_femur");
-		mech_tifi = new MechModel("mech_tifi");
-		
 	    femur = new FemModel3d("femur");
 	    tifi = new FemModel3d("tifi");
 		
@@ -48,51 +51,42 @@ public class lowerlimb extends RootModel {
         }
         
         //Set physical properties
+        femur.setDensity(1900);
+        femur.setMassDamping(0.01);
+        femur.setStiffnessDamping(0.02);
+        femur.setMaterial(new LinearMaterial(1e9, 0.3));
+        
+        //Set physical properties
+        tifi.setDensity(1900);
+        tifi.setMassDamping(0.01);
+        tifi.setStiffnessDamping(0.02);
+        tifi.setMaterial(new LinearMaterial(1e9, 0.3));
+        
+        //Fix of Node
+        try {
+        	ArrayList<FemNode3d> femur_Nodes = NodeNumberReader.read(geodata+"FixNode.txt", femur);
+        	for (FemNode3d FixNode : femur_Nodes) {
+        		if (FixNode!=null) {
+        			FixNode.setDynamic(false);
+        		}
+        	}
+        }
+		catch(IOException e) {
+			System.out.println("Couldn`t read node file" + geodata + "FixNode.txt");
+		}
         
         //add models
-        mech_femur.addModel(femur);
-        mech_tifi.addModel(tifi);
+        mech.addModel(femur);
+        mech.addModel(tifi);
         
-        addModel(mech_femur);
-        addModel(mech_tifi);
+        mech.setCollisionBehavior(femur,tifi,true);
+        
+        addModel(mech);
         
         //set render properties
         setRenderProps (femur);
         setRenderProps (tifi);
-    
-        
-//        Rendering and Visualizations
-//        RenderProps.setVisible(femur, true);
-        
-//        Nodes
-//        RenderProps.setSphericalPoints(femur, 0.1, Color.RED);
-        
-//        Elements
-//        RenderProps.setLineColor(femur, Color.CYAN);
-//        RenderProps.setLineWidth(femur, 2);
-        
-//        femur.setElementWidgetSize (0.7);
-//        RenderProps.setFaceColor (femur, new Color(0.7f, 0.7f, 1f));
-//        RenderProps.setLineWidth(femur, 0);
-
-//        Surface and other meshes
-//        femur.setSurfaceRendering(SurfaceRender.Shaded);
-//        RenderProps.setFaceColor(femur, new Color (0.7f, 0.7f, 1f));
-//        RenderProps.setDrawEdges (femur, true);
-//        RenderProps.setEdgeColor (femur, Color.BLUE);
-        
-//        femur.setSurfaceRendering(SurfaceRender.Stress);
-//        femur.setStressPlotRanging(Ranging.Auto);
-//        RenderProps.setDrawEdges (femur, true);
-//        RenderProps.setEdgeColor (femur, Color.BLUE);
-        
-//        Create a colorbar
-//        ColorBar cbar = new ColorBar();
-//        cbar.setName("colorBar");
-//        cbar.setNumberFormat("%.2f");      // 2 decimal places
-//        cbar.populateLabels(0.0, 1.0, 10); // Start with range [0,1], 10 ticks
-//        cbar.setLocation(-100, 0.1, 20, 0.8);
-//        addRenderable(cbar);      
+           
 	}
 	
 	protected void setRenderProps (FemModel3d fem) {
@@ -101,14 +95,4 @@ public class lowerlimb extends RootModel {
 		RenderProps.setFaceColor(fem, new Color (0.5f, 0.5f, 1f));
 	}
 	
-//	@Override
-//	public void prerender(RenderList list) {
-//		super.prerender(list);
-//		// Synchronize color bar/values in case they are changed. Do this *after*
-//		// super.prerender(), in case values are changed there.
-//		ColorBar cbar = (ColorBar)(renderables().get("colorBar"));
-//		cbar.setColorMap(femur.getColorMap());
-//		DoubleInterval range = femur.getStressPlotRange();
-//		cbar.updateLabels(range.getLowerBound(), range.getUpperBound());
-//	}
 }
