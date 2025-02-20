655e6131906ca1b945f0d00e03b3eb2e3a7aa89e
WiskerJan <ky8350135@gmail.com>
2025-02-20 17:21:43 +0100

    add a FemurCart FEM model

--- src/Knee_model_rigidbody/Knee_model_rigidbody.java
+++ src/Knee_model_rigidbody/Knee_model_rigidbody.java
@@ -2,9 +2,13 @@ package Knee_model_rigidbody;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import artisynth.core.femmodels.AnsysCdbReader;
+import artisynth.core.femmodels.FemElement3dBase;
 import artisynth.core.femmodels.FemModel3d;
+import artisynth.core.femmodels.FemNode3d;
 import artisynth.core.femmodels.FemModel.Ranging;
 import artisynth.core.femmodels.FemModel.SurfaceRender;
 import artisynth.core.gui.ControlPanel;
@@ -18,24 +22,28 @@ import artisynth.core.mechmodels.FrameMarker;
 import artisynth.core.mechmodels.GimbalJoint;
 import artisynth.core.mechmodels.JointBase;
 import artisynth.core.mechmodels.MechModel;
+import artisynth.core.mechmodels.PointFrameAttachment;
 import artisynth.core.mechmodels.RigidBody;
 import artisynth.core.probes.NumericInputProbe;
+import artisynth.core.renderables.ColorBar;
 import artisynth.core.workspace.RootModel;
 import maspack.geometry.PolygonalMesh;
 import maspack.matrix.Point3d;
 import maspack.matrix.RigidTransform3d;
 import maspack.matrix.Vector3d;
 import maspack.matrix.VectorNd;
+import maspack.render.RenderList;
 import maspack.render.RenderProps;
 import maspack.render.Renderer.AxisDrawStyle;
+import maspack.util.DoubleInterval;
 import maspack.util.PathFinder;
 
 public class Knee_model_rigidbody extends RootModel {
 
 	String Modeldata = maspack.util.PathFinder.getSourceRelativePath(this, "data/");
 	MechModel mech = new MechModel();
-	RigidBody Femur, Meniscus, TibiaFibula, Patella, PatellaCart;
-	FemModel3d FemurCart, TibiaCart;
+	RigidBody Femur, Meniscus, TibiaFibula, TibiaCart, Patella, PatellaCart;
+	FemModel3d FemurCart;
 	JointBase Joint;
 
 	public void build(String[] args) throws IOException {
@@ -50,35 +58,45 @@ public class Knee_model_rigidbody extends RootModel {
 		TibiaFibula = importRigidBody("tibia.obj", "TibiaFibula", 2.1e-5);
 		Patella = importRigidBody("patella.obj", "Patella", 2.1e-5);
 		// FemurCart = importRigidBody("femcart.obj", "FemurCart", 2.1e-5);
-		// TibiaCart = importRigidBody("tibcart.obj", "TibiaCart", 2.1e-5);
+		TibiaCart = importRigidBody("tibcart.obj", "TibiaCart", 2.1e-5);
 		Meniscus = importRigidBody("Meniscus.obj", "Meniscus", 2.1e-5);
-		// PatellaCart = importRigidBody("patcart.obj", "PatellaCart", 2.1e-5);
+		PatellaCart = importRigidBody("patcart.obj", "PatellaCart", 2.1e-5);
 
 		// Set rigid body as non-dynamic
 		TibiaFibula.setDynamic(false);
 
-		// import fem model
-		FemurCart = importFemModel("mesh_FemurCart.cdb", "FemurCart", 2.1e-10, 0.02, 1e-6,
-				new LinearMaterial(1, 0.3));
+		// import FEM model
+		FemurCart = importFemModel("mesh_FemurCart.cdb", "FemurCart", 2.1e-5, 0.02, 1e-6, new LinearMaterial(1, 0.45));
+
+		// connecting FEM Model to Rigid Body model
+		for (FemNode3d n : FemurCart.getNodes()) {
+			if (FemurCart.isSurfaceNode(n)) {
+				if (n.getPosition().y > 1390) {
+					RenderProps.setVisible(n, true);
+					RenderProps.setSphericalPoints(n, 0.1, Color.red);
+					mech.attachPoint(n, Femur);
+				}
+			}
+		}
 
 		// set joint
 		Joint = createJoint(Femur, TibiaFibula);
 
 		// Set collision behaviors
-		setCollisionBehavior(Femur, 1e-6, 1e6);
-		setCollisionBehavior(TibiaFibula, 1e-6, 1e6);
-		setCollisionBehavior(Patella, 1e-6, 1e6);
-		// setCollisionBehavior(FemurCart, 1e-6, 0.5);
-		// setCollisionBehavior(TibiaCart, 1e-6, 1e6);
-		setCollisionBehavior(Meniscus, 1e-6, 1e6);
-		// setCollisionBehavior(PatellaCart, 1e-6, 0.5);
+		setCollisionBehavior(Femur, 0.01, 1e-6, 1e6);
+		setCollisionBehavior(TibiaFibula, 0.01, 1e-6, 1e6);
+		setCollisionBehavior(Patella, 0.01, 1e-6, 1e6);
+		setCollisionBehavior(FemurCart, 0.01, 1e-6, 0.5);
+		setCollisionBehavior(TibiaCart, 0.01, 1e-6, 1e6);
+		setCollisionBehavior(Meniscus, 0.01, 1e-6, 1e6);
+		setCollisionBehavior(PatellaCart, 0.01, 1e-6, 0.5);
 
 		// Enable collision force visualization
 		setCollisionManager();
 
 		// add a Input Probe
 		createInputProbe();
-		
+
 		// set a stop time
 		addBreakPoint(5.0);
 	}
@@ -91,18 +109,15 @@ public class Knee_model_rigidbody extends RootModel {
 		return body;
 	}
 
-	// import Fem Model
+	// import FEM Model
 	private FemModel3d importFemModel(String filename, String name, double density, double massDamping,
 			double stiffnessDamping, FemMaterial material) throws IOException {
-		// Import model
 		FemModel3d femModel = AnsysCdbReader.read(Modeldata + filename);
-		// Set properties
 		femModel.setDensity(density);
 		femModel.setMassDamping(massDamping);
 		femModel.setStiffnessDamping(stiffnessDamping);
 		femModel.setMaterial(material);
 		femModel.setName(name);
-		// Add model to the mechanical system
 		mech.addModel(femModel);
 		setFemRenderProps(femModel);
 		return femModel;
@@ -112,20 +127,44 @@ public class Knee_model_rigidbody extends RootModel {
 	private void setFemRenderProps(FemModel3d fem) {
 		// fem.setSurfaceRendering(SurfaceRender.Stress);
 		// fem.setStressPlotRanging(Ranging.Auto);
-		RenderProps.setFaceColor (fem, Color.LIGHT_GRAY);
-		RenderProps.setAlpha(fem, 1.0);
-		RenderProps.setVisible(fem.getNodes(), false);
-		RenderProps.setVisible(fem.getElements(), true);
-		RenderProps.setLineColor(fem, Color.darkGray);
+		// RenderProps.setVisible(fem.getNodes(), false);
+		// RenderProps.setVisible(fem.getElements(), false);
+		// RenderProps.setAlpha(fem, 1.0);
+		fem.setSurfaceRendering(SurfaceRender.Shaded);
+		// RenderProps.setFaceColor (fem, Color.GRAY);
+		RenderProps.setLineColor(fem, Color.DARK_GRAY);
 		// RenderProps.setSphericalPoints (fem, 0.2, Color.CYAN);
+
+		// create a color bar
+		/*
+		 * ColorBar cbar = new ColorBar(); 
+		 * cbar.setName("colorBar");
+		 * cbar.setNumberFormat("%.2f"); 
+		 * cbar.populateLabels(0.0, 0.1, 10);
+		 * cbar.setLocation(-100, 0.1, 20, 0.8); 
+		 * addRenderable(cbar);
+		*/
 	}
+	
+	/*
+	public void prerender(RenderList list) {
+		super.prerender(list);
+		ColorBar cbar = (ColorBar) (renderables().get("colorBar"));
+		List<FemModel3d> femModels = Arrays.asList(FemurCart);
+		for (FemModel3d fem : femModels) {
+			cbar.setColorMap(fem.getColorMap());
+			DoubleInterval range = fem.getStressPlotRange();
+			cbar.updateLabels(range.getLowerBound(), range.getUpperBound());
+		}
+	}
+	*/
 
 	// set collision Behavior
-	private void setCollisionBehavior(RigidBody body, double compliance, double damping) {
-		CollisionBehavior behavior = new CollisionBehavior(true, 0.05);
+	private void setCollisionBehavior(Collidable Model, double mu, double compliance, double damping) {
+		CollisionBehavior behavior = new CollisionBehavior(true, mu);
 		behavior.setCompliance(compliance);
 		behavior.setDamping(damping);
-		mech.setCollisionBehavior(body, Collidable.All, behavior);
+		mech.setCollisionBehavior(Model, Collidable.All, behavior);
 	}
 
 	// set collision manager
@@ -137,7 +176,7 @@ public class Knee_model_rigidbody extends RootModel {
 		cm.setContactForceLenScale(0.1);
 		RenderProps.setVisible(cm, true);
 		RenderProps.setSolidArrowLines(cm, 0.2, Color.RED);
-		RenderProps.setSphericalPoints(cm, 1, Color.GREEN);
+		RenderProps.setSphericalPoints(cm, 0.5, Color.GREEN);
 	}
 
 	// create a InputProbe
